b2c9a2f53e1df7f9d8222e9cc92b59fa6a2e3de4
Jugal Modi <119117599+jugalmodi0111@users.noreply.github.com>
2025-11-16 11:10:15 +0530

    Update Introduction.py

--- 03. Data Structures/Arrays/Introduction.py
+++ 03. Data Structures/Arrays/Introduction.py
@@ -18,29 +18,12 @@ array = [5,8,2,9,17,43,25,10]
 first_element = array[0]  #This will return the first element of the array, in this case, 5, in O(1) time
 sixth_element = array[5]  #sixth-element = 43 Again, in O(1) time
 
-
-#Push/Pop
-#Push corresponds to pushing or adding an element at the end of the array.
-#Similarly, pop corresponds to removing the element at the end of the array.
-#Since the index of the end of the array is known, finding it and pushing or popping an element will only require O(1) time
 array.append(87) #Adds 87 at the end of the array in O(1) time
-
-#In some special cases, the append(push) operation may take greater time. This is because as mentioned earlier, Python has dynamic arrays
-#So when an element is to appended and the array is filled, the entire array has to be copied to a new location
-#With more space allocated(generally double the space) this time so that more items can be appended.
-#Therefore, some individual operations may reuire O(n) time or greater, but when averaged over a large number of operations,
-#The complexity can be safely considered to be O(1)
-
 array.pop() #Pops/removes the element at the end of the array in O(1) time.
-
 print(array)
 
 
 #Insert
-#Insert operation inserts an element at the beginning of the array, or at any location specified.
-#This is O(n) operation since after inserting the element at the desired location,
-#The elements to the right of the array have to be updated with the correct index as they all have shifted by one place.
-#This requires looping through the array. Hence, O(n) time.
 array.insert(0,50) #Inserts 50 at the beginning of the array and shifts all other elements one place towards right. O(n)
 array.insert(4,0) #inserts '0' at index '4', thus shifting all elements starting from index 4 one place towards right. O(n)
 
